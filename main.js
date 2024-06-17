f9f3e97696e521489571cde84d4d51c01c16b0d9
rasha200 <yaseenrasha4@gmail.com>
2024-06-17 20:37:20 +0300

    js taske 2

---
+++ main.js
@@ -0,0 +1,130 @@
+//taske 1
+let nn="";
+for(let i=1 ; i<11 ; i++){
+nn+=i+' ';
+// في كل عملية تكرار بنضيف الرقم الحالي لل  variable مع مسافة بعده
+};
+console.log(nn);
+
+//taske 2
+let oo=""
+let x=1;
+while(x<11){
+    oo+=x+' '
+    x++;
+};
+console.log(oo);
+
+
+//taske 4
+let vv="";
+for(y=0 ; y< 11 ; y+=2){
+vv+=y +' '
+};
+console.log(vv);
+
+//taske 5
+sum=0
+for (let i=1 ;i<11 ;i++){
+    sum += i;
+}
+console.log(sum)
+
+
+//taske 6
+let odd="";
+for(r=1 ; r<11 ; r+=2){
+    odd+= r +' '
+};
+console.log(odd);
+
+//taske 7
+let age=26;
+if(age>16){
+    console.log("You are an adult");
+};
+
+
+//taske 8
+let score=45;
+if (score>50){
+    console.log("You pass the test");
+}
+else if(score<=50){
+  console.log("You failed the test");
+};
+
+
+//taske 9
+let nam="John";
+if (nam="Juhn" ){
+    console.log("Hello,John");
+};
+
+
+//taske 10
+let day="Monday";
+if( day!="friday" && day!="saturday"){
+    console.log("It's a weekday")
+}
+
+
+//taske 11
+let numb=7;
+if (numb%2==0){
+    console.log("The number is even");
+}
+else {
+    console.log("The number is not even")
+};
+
+
+
+//taske 12
+let gg=5;
+if(gg>=0){
+    console.log("gg is a positive number")
+}
+else {
+    console.log("gg is a negative number") 
+}
+
+
+//taske 13
+let ne=-5;
+if(ne>=0){
+    console.log("ne is a positive number")
+}
+else {
+    console.log("ne is a negative number") 
+}
+
+
+//taske 14
+let gpa=3.5
+if (gpa>=3.0){
+    console.log("Congratulations, you have a good GPA!")
+}
+else {
+    console.log(" you do not have a good GPA")
+};
+
+
+
+//taske 14
+let kk=7;
+let nb=5;
+if (kk<=1){
+    console.log('false,not prime')
+}
+for(let i=2 ;i<7 ; i++){
+    if (kk%i==0){
+        console.log('false,not prime')
+        break
+    }
+   nb=9
+}
+if(nb==9){
+    console.log('true')
+}
+//????????????????????????????????????????????
